1af7b2db25e375371fad1506e107982c6f6f354d
Bruno de Biazio <biazzioxbox@gmail.com>
2025-05-23 12:20:02 -0300

    já aparece os produtos, falta add botao comprar

--- react/components/Bundle.tsx
+++ react/components/Bundle.tsx
@@ -1,12 +1,48 @@
-import React from "react"
+import React, { useEffect }  from "react"
 import { useProduct } from 'vtex.product-context'
+import { useQuery } from 'react-apollo'
+import { Button } from 'vtex.styleguide'
+
+import ACCESSORIES from '../graphql/getAcessories.graphql'
+
+interface Accessories {
+  productName: string
+}
 
 function Bundle() {
   const productContextValue = useProduct()
+  const {loading, data} = useQuery(ACCESSORIES, {
+    variables: {
+      productId: "199"
+    }
+  })
 
-  console.log('productContextValue', productContextValue)
+  useEffect(() => {
+    if(loading) return
 
-  return (<div>Teste components</div>)
+    console.log('productContextValue', productContextValue)
+    console.log('data', data)
+
+  },[loading, data])
+
+
+
+  return (
+    <>
+      <p>Complemente o produto:</p>
+      <h3>{productContextValue?.selectedItem?.nameComplete}</h3>
+      <p>com:</p>
+      {
+        !loading && data?.productRecommendations.length > 0
+          ? data?.productRecommendations.map(
+            (item: Accessories) => (
+              <Button variation="secondary">{item.productName}</Button>
+            ))
+          : ""
+
+      }
+    </>
+  )
 }
 
 export default Bundle
