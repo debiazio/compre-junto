2c579fbb6c8f2f750f89e9cce1f2b42d882c112b
Bruno de Biazio <biazzioxbox@gmail.com>
2025-05-26 17:05:30 -0300

    finalizado como o vídeo

--- react/components/Bundle.tsx
+++ react/components/Bundle.tsx
@@ -1,21 +1,41 @@
-import React, { useEffect }  from "react"
+import React, { useEffect, useState }  from "react"
 import { useProduct } from 'vtex.product-context'
 import { useQuery } from 'react-apollo'
 import { Button } from 'vtex.styleguide'
+import { Wrapper } from 'vtex.add-to-cart-button'
+import { useCssHandles } from 'vtex.css-handles'
 
-import ACCESSORIES from '../graphql/getAcessories.graphql'
+import ACCESSORIES from '../graphql/getAccessories.graphql'
+
+const CSS_HANDLES = [
+  'ButtonBundle'
+] as const
 
 interface Accessories {
   productName: string
+  items: itemId[]
+}
+
+interface itemId{
+  itemId: string
+}
+
+interface itemsArray {
+  id: string | undefined
+  quantity: number | undefined
+  seller: string
 }
 
 function Bundle() {
   const productContextValue = useProduct()
+  const [ items, setitems ] = useState<itemsArray[]>()
   const {loading, data} = useQuery(ACCESSORIES, {
     variables: {
       productId: "199"
     }
   })
+
+  const { handles } = useCssHandles(CSS_HANDLES)
 
   useEffect(() => {
     if(loading) return
@@ -23,9 +43,36 @@ function Bundle() {
     console.log('productContextValue', productContextValue)
     console.log('data', data)
 
+    const initialItem: itemsArray[] = [
+      {
+        id: productContextValue?.selectedItem?.itemId,
+        quantity: productContextValue?.selectedQuantity,
+        seller: "1"
+      }
+
+    ]
+
+
+    setitems(initialItem)
+
   },[loading, data])
 
+  useEffect(() => {
 
+    console.log('items', items)
+  }, [items])
+
+  function addItem (itemId: string) {
+    const newItem = [
+      {
+        id: itemId,
+        quantity: 1,
+        seller: "1"
+      }
+    ]
+
+    setitems(items?.concat(newItem))
+  }
 
   return (
     <>
@@ -36,11 +83,18 @@ function Bundle() {
         !loading && data?.productRecommendations.length > 0
           ? data?.productRecommendations.map(
             (item: Accessories) => (
-              <Button variation="secondary">{item.productName}</Button>
+              <div className={`${handles.ButtonBundle}`}>
+              <Button
+                  variation="secondary"
+                  onClick={() => addItem(item.items[0].itemId)}
+                >{item.productName}</Button>
+                </div>
             ))
           : ""
 
       }
+      <Wrapper
+        skuItems={items}/>
     </>
   )
 }
